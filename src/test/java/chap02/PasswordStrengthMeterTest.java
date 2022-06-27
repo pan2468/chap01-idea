ebfa2f2a5b6f1f16561215dc33d6e9938c75db4e
임채성 <pan1129@naver.com>
2022-06-27 13:50:40 +0900

    문자열 테스트 Commit

---
+++ src/test/java/chap02/PasswordStrengthMeterTest.java
@@ -0,0 +1,20 @@
+package chap02;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PasswordStrengthMeterTest {
+
+    @Test
+    void name(){
+
+    }
+
+    @Test
+    void meetsAllCriteria_Then_Strong(){
+        PasswordStrengthMeter meter = new PasswordStrengthMeter();
+        PasswordStrength result = meter.meter("ab12!@AB");
+        assertEquals(PasswordStrength.STRONG,result);
+    }
+}
