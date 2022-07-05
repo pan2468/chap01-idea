7edbe0d240a0e3e4f5b17c422cbd8fab5c464eb7
임채성 <pan1129@naver.com>
2022-07-05 13:57:28 +0900

    기능 검증 메소드 Commit

---
+++ src/test/java/chap10/BasicTest.java
@@ -0,0 +1,18 @@
+package chap10;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class BasicTest {
+
+    @Test
+    void sumTest(){
+        int value = sum(2,2);
+        assertThat(value).isEqualTo(4);
+    }
+
+    private int sum(int a, int b){
+        return a + b;
+    }
+}
